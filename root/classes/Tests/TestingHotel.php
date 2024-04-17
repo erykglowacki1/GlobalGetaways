6cec9d8da51cd29fd3ed49f0909c0bc7894b5d36
erykglowacki1 <B00153656@mytudublin.ie>
2024-04-17 23:10:14 +0100

    added folder for testing docs

--- root/classes/Tests/TestingHotel.php
+++ root/classes/Tests/TestingHotel.php
@@ -1,11 +1,17 @@
 <?php
 require "../Hotel.php";
+/////////UNIT TEST FOR HOTEL CLASS/////////////
+//$hotel = new Hotel(null,null,null,null);
+//$hotel->setHotelName("Ritz Carlton");
+//$hotel->setNumRooms(10);
+//$hotel->setPrice(2000);
+//
+//$hotel->displayTestHotels();
 
-$hotel = new Hotel(null,null,null,null);
-$hotel->setHotelName("Ritz Carlton");
-$hotel->setNumRooms(10);
-$hotel->setPrice(2000);
+$hotel1 = new Hotel(null,null,null,null);
+$hotel1->setHotelName();
+$hotel1->setNumRooms(10);
+$hotel1->setPrice(2000);
 
-$hotel->displayTestHotels();
-
+$hotel1->displayTestHotels();
 
---
+++ root/classes/Tests/TestingPayment.php
@@ -0,0 +1,2 @@
+<?php
+require "../Payment.php";
