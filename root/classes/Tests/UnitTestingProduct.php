f1b9a74a18232e096124e73ef9610464de334904
erykglowacki1 <B00153656@mytudublin.ie>
2024-04-28 21:57:56 +0100

    flight fixed

--- root/classes/Tests/UnitTestingProduct.php
+++ root/classes/Tests/UnitTestingProduct.php
@@ -1,9 +1,8 @@
 <?php
 require "../Product.php";
 
-$productUnitTest = new Product(0,0,0,0,0);
+$productUnitTest = new Product(0,0,0,0);
 $productUnitTest->setProductId(100);
-$productUnitTest->setFlightId(12);
 $productUnitTest->setActivityId(13);
 $productUnitTest->setHotelId(14);
 $productUnitTest->setDestinationId(15);
